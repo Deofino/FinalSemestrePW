487e0a8dc8ca1907715ef89700babdf97989b210
gDelps <guilhermedelfino25@gmail.com>
2021-06-08 14:25:00 -0300

    Definindo constantes, requirindo Twig...

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+
+    // Endereco principals
+    define("URL_MAIN", 'http://localhost/Escola/2b/FinalSemestrePW/');
+    // define("URL_MAIN", 'url main remote');
+    
+    // Nome do banco de dados
+    define("DB_NAME", 'name');
+    // define("DB_NAME", 'remote name');
+    
+    // Usuario no Mysql
+    define("DB_USER", 'root');
+    // define("DB_USER", 'user remote');
+    
+    // Senha no Mysql
+    define("DB_PASSWORD", '');
+    // define("DB_PASSWORD", 'password remote');
+    
+    // Host na maquina Local
+    define("HOST", 'localhost');
+    // define("HOST", 'host remote');
+
