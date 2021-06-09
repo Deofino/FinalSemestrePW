075027b882689e31b03012b71010c8fcfa8ddf25
Vitória Gonçalves Passos <vitoriaGoncalves08@users.noreply.github.com>
2021-06-09 13:21:48 -0300

    imagens aqui

---
+++ app/view/cache/68/68722edf937dc161d8cba9ffb1929a105a7c29cae9bdc27c6dcb12fc89b3bf9e.php
@@ -0,0 +1,61 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* ./footer.twig */
+class __TwigTemplate_0d86fbe144c192664c79e7fd62f938c7301699207937d9149121b12276ec7eb7 extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<div style=\"width: 100%;height: 200px;background-color: #ccc;padding: 5px;\">
+    <ul>
+        <li><a href=\"#\">FOOTER</a></li>
+        <li><a href=\"#\">FOOTER</a></li>
+        <li><a href=\"#\">FOOTER</a></li>
+        <li><a href=\"#\">FOOTER</a></li>
+    </ul>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "./footer.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "./footer.twig", "C:\\xampp\\htdocs\\3MtecA\\08-06\\FinalSemestrePW\\app\\view\\template\\footer.twig");
+    }
+}
