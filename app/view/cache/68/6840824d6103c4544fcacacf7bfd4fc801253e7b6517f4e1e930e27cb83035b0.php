7f2bd59807dfdf11880ad230490522b221c8b2e0
Vitória Gonçalves Passos <vitoriaGoncalves08@users.noreply.github.com>
2021-06-23 12:38:14 -0300

    falta arrumar o card

--- app/view/cache/68/6840824d6103c4544fcacacf7bfd4fc801253e7b6517f4e1e930e27cb83035b0.php
+++ app/view/cache/68/6840824d6103c4544fcacacf7bfd4fc801253e7b6517f4e1e930e27cb83035b0.php
@@ -88,60 +88,62 @@ class __TwigTemplate_82f16233403e1f0a25dfb3a8607e77a0d2523b0dbfca433972f9ec32463
     </section>
 
     <!-- CARDS AQUI  -->
-    <div class=\"grid\">
-        <div class=\"grid-item\">
-          <div class=\"card\">
-            <img class=\"card-img\" src=\"";
-        // line 24
+    <div class=\"cards_content\">
+      <div class=\"grid\">
+          <div class=\"grid-item\">
+            <div class=\"card\">
+              <img class=\"card-img\" src=\"";
+        // line 25
         echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
         echo "img/c.jpg\" alt=\"Rome\" />
-            <div class=\"card-content\">
-              <h1 class=\"card-header\">Adidas seilaoque</h1>
-            
-              <p class=\"card-text\">
-               <strong> Casual </strong>
-               </p>
-                <p class=\"card-text\">Cor: Branco</p>
-                <p class=\"card-text\">R\$:200,00</p>
-              <button class=\"card-btn\">Visit <span>&rarr;</span></button>
+              <div class=\"card-content\">
+                <h1 class=\"card-header\">Adidas seilaoque</h1>
+              
+                <p class=\"card-text\">
+                <strong> Casual </strong>
+                </p>
+                  <p class=\"card-text\">Cor: Branco</p>
+                  <p class=\"card-text\">R\$:200,00</p>
+                <button class=\"card-btn\">Visit <span>&rarr;</span></button>
+              </div>
             </div>
           </div>
-        </div>
-        <div class=\"grid-item\">
-          <div class=\"card\">
-            <img
-              class=\"card-img\"
-              src=\"";
-        // line 41
+          <div class=\"grid-item\">
+            <div class=\"card\">
+              <img
+                class=\"card-img\"
+                src=\"";
+        // line 42
         echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
         echo "img/c.jpg\"
-              alt=\"Grand Canyon\"
-            />
-            <div class=\"card-content\">
-              <h1 class=\"card-header\">Grand Canyon</h1>
-              <p class=\"card-text\">
-                One of the world's natural wonders, the iconic Grand Canyon draws
-                oohs and aahs from visitors perched at the edge of its
-                <strong>towering cliffs</strong>.
-              </p>
-              <button class=\"card-btn\">Visit <span>&rarr;</span></button>
+                alt=\"Grand Canyon\"
+              />
+              <div class=\"card-content\">
+                <h1 class=\"card-header\">Grand Canyon</h1>
+                <p class=\"card-text\">
+                  One of the world's natural wonders, the iconic Grand Canyon draws
+                  oohs and aahs from visitors perched at the edge of its
+                  <strong>towering cliffs</strong>.
+                </p>
+                <button class=\"card-btn\">Visit <span>&rarr;</span></button>
+              </div>
             </div>
           </div>
-        </div>
-        <div class=\"grid-item\">
-          <div class=\"card\">
-            <img class=\"card-img\" src=\"";
-        // line 57
+          <div class=\"grid-item\">
+            <div class=\"card\">
+              <img class=\"card-img\" src=\"";
+        // line 58
         echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
         echo "img/c.jpg\" alt=\"Maldives\" />
-            <div class=\"card-content\">
-              <h1 class=\"card-header\">Maldives</h1>
-              <p class=\"card-text\">
-                The Maldives are known for their
-                <strong>natural environment</strong> including the blue ocean,
-                white beaches, and clean air, attracting tourists.
-              </p>
-              <button class=\"card-btn\">Visit <span>&rarr;</span></button>
+              <div class=\"card-content\">
+                <h1 class=\"card-header\">Maldives</h1>
+                <p class=\"card-text\">
+                  The Maldives are known for their
+                  <strong>natural environment</strong> including the blue ocean,
+                  white beaches, and clean air, attracting tourists.
+                </p>
+                <button class=\"card-btn\">Visit <span>&rarr;</span></button>
+              </div>
             </div>
           </div>
         </div>
@@ -160,11 +162,11 @@ class __TwigTemplate_82f16233403e1f0a25dfb3a8607e77a0d2523b0dbfca433972f9ec32463
 ";
     }
 
-    // line 82
+    // line 84
     public function block_scripts($context, array $blocks = [])
     {
         $macros = $this->macros;
-        // line 83
+        // line 85
         echo "    
 ";
     }
@@ -181,7 +183,7 @@ class __TwigTemplate_82f16233403e1f0a25dfb3a8607e77a0d2523b0dbfca433972f9ec32463
 
     public function getDebugInfo()
     {
-        return array (  168 => 83,  164 => 82,  135 => 57,  116 => 41,  96 => 24,  81 => 11,  77 => 10,  70 => 8,  66 => 7,  61 => 5,  57 => 4,  50 => 3,  39 => 1,);
+        return array (  170 => 85,  166 => 84,  136 => 58,  117 => 42,  97 => 25,  81 => 11,  77 => 10,  70 => 8,  66 => 7,  61 => 5,  57 => 4,  50 => 3,  39 => 1,);
     }
 
     public function getSourceContext()
