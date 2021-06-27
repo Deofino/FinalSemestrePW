8ac3344aaab99b4411893f469603f28b388fa850
Vitória Gonçalves Passos <vitoriaGoncalves08@users.noreply.github.com>
2021-06-27 00:12:52 -0300

    arrumando as cores do crud

--- app/view/cache/68/6840824d6103c4544fcacacf7bfd4fc801253e7b6517f4e1e930e27cb83035b0.php
+++ app/view/cache/68/6840824d6103c4544fcacacf7bfd4fc801253e7b6517f4e1e930e27cb83035b0.php
@@ -46,19 +46,21 @@ class __TwigTemplate_82f16233403e1f0a25dfb3a8607e77a0d2523b0dbfca433972f9ec32463
         $this->parent->display($context, array_merge($this->blocks, $blocks));
     }
 
-    // line 3
     public function block_title($context, array $blocks = [])
     {
         $macros = $this->macros;
         echo "Shoes | Inicio ";
     }
 
-    // line 4
+    // line 2
     public function block_metas_data($context, array $blocks = [])
     {
         $macros = $this->macros;
-        // line 5
-        echo "  <meta name=\"description\" content=\"A melhor loja de todas para se encontrar tenis e sapatos lindissimos para programadores ainda mais lindissimos, com muito orgulho apresento a vocês... SHOES!\">
+        // line 3
+        echo "<meta
+  name=\"description\"
+  content=\"A melhor loja de todas para se encontrar tenis e sapatos lindissimos para programadores ainda mais lindissimos, com muito orgulho apresento a vocês... SHOES!\"
+/>
 ";
     }
 
@@ -67,108 +69,113 @@ class __TwigTemplate_82f16233403e1f0a25dfb3a8607e77a0d2523b0dbfca433972f9ec32463
     {
         $macros = $this->macros;
         // line 8
-        echo "    <link rel=\"stylesheet\" href=\"";
+        echo "<link rel=\"stylesheet\" href=\"";
         echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
-        echo "css/index.css\">
+        echo "css/index.css\" />
 ";
     }
 
-    // line 10
+    // line 9
     public function block_body($context, array $blocks = [])
     {
         $macros = $this->macros;
-        // line 11
+        // line 10
         echo "
-     <section>
-        <h1>Tênis incríveis</h1>
-    </section>
-    <section>
-        <h1 class=\"tilt1\">SHOES</h1>
-        <p>Este é nosso projeto realizado para menção do primeiro semestre da matéria de Projeto WEB III, desenvolvido com as features solicitadas pelo lecionador de conteúdo Allan. Desejamos satisfazer e conseguirmos conquistar um belo de um MB pra nossa conta... A aplicação será em volta de uma apresentação de Sapatos e Tênis, semelhante a um E-Commerce, podendo mostrar assim produtos de uma loja de sapatos física que até o momento não tem certeza se quer investir nisso para poder criar um E-commerce completo, sendo assim, um site mais voltado para o marketing da loja.</p>
-    </section>
+<section>
+  <h1>Tênis incríveis</h1>
+</section>
+<section>
+  <h1 class=\"tilt1\">SHOES</h1>
+  <p>
+    Este é nosso projeto realizado para menção do primeiro semestre da matéria
+    de Projeto WEB III, desenvolvido com as features solicitadas pelo lecionador
+    de conteúdo Allan. Desejamos satisfazer e conseguirmos conquistar um belo de
+    um MB pra nossa conta... A aplicação será em volta de uma apresentação de
+    Sapatos e Tênis, semelhante a um E-Commerce, podendo mostrar assim produtos
+    de uma loja de sapatos física que até o momento não tem certeza se quer
+    investir nisso para poder criar um E-commerce completo, sendo assim, um site
+    mais voltado para o marketing da loja.
+  </p>
+</section>
 
-    <!-- CARDS AQUI  -->
-    <div class=\"cards_content\">
-      <div class=\"grid\">
-          <div class=\"grid-item\">
-            <div class=\"card\">
-              <img class=\"card-img\" src=\"";
-        // line 25
-        echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
-        echo "img/c.jpg\" alt=\"Rome\" />
-              <div class=\"card-content\">
-                <h1 class=\"card-header\">Adidas seilaoque</h1>
-              
-                <p class=\"card-text\">
-                <strong> Casual </strong>
-                </p>
-                  <p class=\"card-text\">Cor: Branco</p>
-                  <p class=\"card-text\">R\$:200,00</p>
-                <button class=\"card-btn\">Visit <span>&rarr;</span></button>
-              </div>
-            </div>
-          </div>
-          <div class=\"grid-item\">
-            <div class=\"card\">
-              <img
-                class=\"card-img\"
-                src=\"";
-        // line 42
-        echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
-        echo "img/c.jpg\"
-                alt=\"Grand Canyon\"
-              />
-              <div class=\"card-content\">
-                <h1 class=\"card-header\">Grand Canyon</h1>
-                <p class=\"card-text\">
-                  One of the world's natural wonders, the iconic Grand Canyon draws
-                  oohs and aahs from visitors perched at the edge of its
-                  <strong>towering cliffs</strong>.
-                </p>
-                <button class=\"card-btn\">Visit <span>&rarr;</span></button>
-              </div>
-            </div>
-          </div>
-          <div class=\"grid-item\">
-            <div class=\"card\">
-              <img class=\"card-img\" src=\"";
-        // line 58
-        echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
-        echo "img/c.jpg\" alt=\"Maldives\" />
-              <div class=\"card-content\">
-                <h1 class=\"card-header\">Maldives</h1>
-                <p class=\"card-text\">
-                  The Maldives are known for their
-                  <strong>natural environment</strong> including the blue ocean,
-                  white beaches, and clean air, attracting tourists.
-                </p>
-                <button class=\"card-btn\">Visit <span>&rarr;</span></button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    <!-- FIM CARDS -->
-    
-    <section></section>
-    <section>
-        <h1 class=\"tilt2\">Estilo versátil pronto para as ruas</h1>
-        <p>Temos tênis de inumeras marcas visadas atualmente e uma garantia de que nossos produtos são originais e de ótima qualidade, se gostou de algum venha conhecer nossa loja.</p>
-       <a href=\"products\"><i class=\"fas fa-long-arrow-alt-right\"></i></a>
-    </section>
+<!-- CARDS AQUI  -->
+<main id=\"l_cards\">
 
-    
-    
+
+  <div class=\"card\">
+    <div class=\"card_header\">
+      <img src=\"";
+        // line 34
+        echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
+        echo "img/c.jpg\" width=\"200\" height=\"200\" alt=\"Fofo\">
+    </div>
+    <div class=\"card_body\">
+      <h2>Adidas Pastel</h2>
+      <!-- <p>Descricao bonita bem cheia Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis odio exercitationem velit perferendis. Placeat possimus illo eius sequi laboriosam nobis vel in quis! Fugit, corrupti temporibus aliquid dolorum quam doloremque.</p> -->
+      <p>Casual</p>
+      <!-- <p>Cor : Branco</p> -->
+      <p>R\$ 290,00</p>
+      <button class=\"details\">Detalhes</button>
+    </div>
+  </div>
+
+   <div class=\"card\">
+    <div class=\"card_header\">
+      <img src=\"";
+        // line 48
+        echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
+        echo "img/home/paralax3.jpg\" width=\"200\" height=\"200\" alt=\"Fofo\">
+    </div>
+    <div class=\"card_body\">
+      <h2>Adidas bonito</h2>
+      <!-- <p>Descricao bonita bem cheia Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis odio exercitationem velit perferendis. Placeat possimus illo eius sequi laboriosam nobis vel in quis! Fugit, corrupti temporibus aliquid dolorum quam doloremque.</p> -->
+      <p>Casual</p>
+      <!-- <p>Cor : Branco</p> -->
+      <p>R\$ 240,00</p>
+      <button class=\"details\">Detalhes</button>
+    </div>
+  </div>
+
+   <div class=\"card\">
+    <div class=\"card_header\">
+      <img src=\"";
+        // line 62
+        echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
+        echo "img/home/paralax1.jpg\" width=\"200\" height=\"200\" alt=\"Fofo\">
+    </div>
+    <div class=\"card_body\">
+      <h2>Nike Air Max</h2>
+      <!-- <p>Descricao bonita bem cheia Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis odio exercitationem velit perferendis. Placeat possimus illo eius sequi laboriosam nobis vel in quis! Fugit, corrupti temporibus aliquid dolorum quam doloremque.</p> -->
+      <p>Casual</p>
+      <!-- <p>Cor : Branco</p> -->
+      <p>R\$ 300,00</p>
+      <button class=\"details\">Detalhes</button>
+    </div>
+  </div>
+
+
+</main>
+<!-- FIM CARDS -->
+
+<section></section>
+<section>
+  <h1 class=\"tilt2\">Estilo versátil pronto para as ruas</h1>
+  <p>
+    Temos tênis de inumeras marcas visadas atualmente e uma garantia de que
+    nossos produtos são originais e de ótima qualidade, se gostou de algum venha
+    conhecer nossa loja.
+  </p>
+  <a href=\"products\"><i class=\"fas fa-long-arrow-alt-right\"></i></a>
+</section>
+
 ";
     }
 
-    // line 84
+    // line 89
     public function block_scripts($context, array $blocks = [])
     {
         $macros = $this->macros;
-        // line 85
-        echo "    
-";
+        echo " ";
     }
 
     public function getTemplateName()
@@ -183,7 +190,7 @@ class __TwigTemplate_82f16233403e1f0a25dfb3a8607e77a0d2523b0dbfca433972f9ec32463
 
     public function getDebugInfo()
     {
-        return array (  170 => 85,  166 => 84,  136 => 58,  117 => 42,  97 => 25,  81 => 11,  77 => 10,  70 => 8,  66 => 7,  61 => 5,  57 => 4,  50 => 3,  39 => 1,);
+        return array (  175 => 89,  143 => 62,  126 => 48,  109 => 34,  83 => 10,  79 => 9,  72 => 8,  68 => 7,  60 => 3,  56 => 2,  39 => 1,);
     }
 
     public function getSourceContext()
---
+++ app/view/cache/73/736795cb7ce089f3d491c74356d5501572bc8ad7b0045d8d5f143eda3ad5e19f.php
@@ -0,0 +1,181 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* ./private/update.twig */
+class __TwigTemplate_d7b98dc173caa59cd20a931b4dcb0dddeb8817278fc4045f86593ef9fe9acbfa extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->blocks = [
+            'metas_data' => [$this, 'block_metas_data'],
+            'title' => [$this, 'block_title'],
+            'styles' => [$this, 'block_styles'],
+            'body' => [$this, 'block_body'],
+            'scripts' => [$this, 'block_scripts'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "./template/html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $this->parent = $this->loadTemplate("./template/html.twig", "./private/update.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_metas_data($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+    }
+
+    // line 5
+    public function block_title($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        echo "Shoes | Excluir
+";
+    }
+
+    // line 8
+    public function block_styles($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 9
+        echo "\t<link rel=\"stylesheet\" href=\"";
+        echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
+        echo "css/update.css\">
+";
+    }
+
+    // line 12
+    public function block_body($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 13
+        echo "
+\t<main id=\"update\">
+
+\t\t";
+        // line 97
+        echo "\t\t";
+        if (($context["products"] ?? null)) {
+            // line 98
+            echo "\t\t\t<h1>Atualizar produto</h1>
+\t\t\t<section class=\"l_table\">
+\t\t\t\t<table>
+\t\t\t\t\t<thead>
+\t\t\t\t\t\t<tr>
+\t\t\t\t\t\t\t<th>ID</th>
+\t\t\t\t\t\t\t<th>Produto</th>
+\t\t\t\t\t\t\t<th>Imagem</th>
+\t\t\t\t\t\t\t<th>Atualizar</th>
+\t\t\t\t\t\t</tr>
+\t\t\t\t\t</thead>
+\t\t\t\t\t<tbody>
+\t\t\t\t\t\t";
+            // line 110
+            $context['_parent'] = $context;
+            $context['_seq'] = twig_ensure_traversable(($context["products"] ?? null));
+            foreach ($context['_seq'] as $context["_key"] => $context["product"]) {
+                // line 111
+                echo "\t\t\t\t\t\t\t<tr>
+\t\t\t\t\t\t\t\t<td>";
+                // line 112
+                echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["product"], "_id", [], "any", false, false, false, 112), "html", null, true);
+                echo "</td>
+\t\t\t\t\t\t\t\t<td>";
+                // line 113
+                echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["product"], "nameShoe", [], "any", false, false, false, 113), "html", null, true);
+                echo "</td>
+\t\t\t\t\t\t\t\t<td><img src=\"";
+                // line 114
+                echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
+                echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["product"], "dirImageShoe", [], "any", false, false, false, 114), "html", null, true);
+                echo "\" alt=\"";
+                echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["product"], "nameShoe", [], "any", false, false, false, 114), "html", null, true);
+                echo "\"></td>
+\t\t\t\t\t\t\t\t<td>
+\t\t\t\t\t\t\t\t\t<a href=\"";
+                // line 116
+                echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
+                echo "product/read/";
+                echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["product"], "_id", [], "any", false, false, false, 116), "html", null, true);
+                echo "\">Modificar</a>
+\t\t\t\t\t\t\t\t</td>
+\t\t\t\t\t\t\t</tr>
+\t\t\t\t\t\t";
+            }
+            $_parent = $context['_parent'];
+            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['product'], $context['_parent'], $context['loop']);
+            $context = array_intersect_key($context, $_parent) + $_parent;
+            // line 120
+            echo "\t\t\t\t\t</tbody>
+\t\t\t\t</table>
+\t\t\t</tbody>
+\t\t</section>
+\t";
+        } else {
+            // line 125
+            echo "\t\t<h1>Sem produtos!!</h1>
+\t";
+        }
+        // line 127
+        echo "
+
+</main>";
+    }
+
+    // line 129
+    public function block_scripts($context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 130
+        echo "<script src=\"";
+        echo twig_escape_filter($this->env, ($context["file"] ?? null), "html", null, true);
+        echo "js/update.js\"></script>";
+    }
+
+    public function getTemplateName()
+    {
+        return "./private/update.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  157 => 130,  153 => 129,  147 => 127,  143 => 125,  136 => 120,  124 => 116,  116 => 114,  112 => 113,  108 => 112,  105 => 111,  101 => 110,  87 => 98,  84 => 97,  79 => 13,  75 => 12,  68 => 9,  64 => 8,  56 => 5,  50 => 3,  39 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "./private/update.twig", "C:\\xampp\\htdocs\\3MtecA\\08-06\\FinalSemestrePW\\app\\view\\private\\update.twig");
+    }
+}
